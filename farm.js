0883338b532f6c6790b7eed9efb4bfa857f7c026
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2020-11-26 14:27:18 +0100

    added the environmentFactor
    to getYieldForPlants

--- farm.js
+++ farm.js
@@ -1,5 +1,5 @@
- 
-const getYieldForPlant = (crop) => crop.yield * 1;
+// functions 
+const getYieldForPlant = (crop, environmentFactors) => getYieldFactor(crop, environmentFactors) * crop.yield;
 const getYieldForCrop = (input) => input.numCrops * input.crop.yield;
 const getTotalYield = ({crops}) => {
     let yield_total = 0;
@@ -12,5 +12,41 @@ const getCostsForCrop = (crop) => crop.numCrops * crop.costs;
 const getRevenueForCrop = (crop) => crop.numCrops * getYieldForPlant(crop) * crop.salesPrice;
 const getProfitForCrop = (crop) => getRevenueForCrop(crop) - getCostsForCrop(crop);
 const getTotalProfit = (crop) => getRevenueForCrop(crop) - getProfitForCrop(crop);
+
+// helpers
+const getYieldFactor = (crop, environmentFactor)=>{
+    let yieldFactor = 1;
+    switch (environmentFactor.sun){
+        case "low":
+            yieldFactor = yieldFactor * (100 + crop.factors.sun.low) / 100;
+            break;
+        case "medium":
+            yieldFactor = yieldFactor * (100 + crop.factors.sun.medium) / 100;
+            break;
+        case "high":
+            yieldFactor = yieldFactor * (100 + crop.factors.sun.high) / 100;
+    }
+    switch (environmentFactor.wind){
+        case "low":
+            yieldFactor = yieldFactor * (100 + crop.factors.wind.low) / 100;
+            break;
+        case "medium":
+            yieldFactor = yieldFactor * (100 + crop.factors.wind.medium) / 100;
+            break;
+        case "high":
+            yieldFactor = yieldFactor * (100 + crop.factors.wind.high) / 100;
+    }
+    switch (environmentFactor.underground){
+        case "clay":
+            yieldFactor = yieldFactor * (100 + crop.factors.underground.clay) / 100;
+            break;
+        case "sand":
+            yieldFactor = yieldFactor * (100 + crop.factors.underground.sand) / 100;
+            break;
+        case "garden":
+            yieldFactor = yieldFactor * (100 + crop.factors.underground.garden) / 100;
+    }
+    return yieldFactor;
+}
 
 module.exports = {getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop, getRevenueForCrop, getProfitForCrop, getTotalProfit};
