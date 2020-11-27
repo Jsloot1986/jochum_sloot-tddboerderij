45b3fcdb3ae37e97d978a4d37317766b18b55d2c
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2020-11-27 13:40:37 +0100

    added environmentFactors
    Added at getYieldforPlant and getYieldForCrop the environmentFactors

--- farm.js
+++ farm.js
@@ -1,6 +1,44 @@
+// helpers
+
+const getYieldFactor = (crop, environmentFactor)=>{
+    let yieldFactor = 1;
+    switch (environmentfactor.sun){
+        case "low": 
+        yieldFactor = yieldFactor * (100 + crop.factors.sun.low) / 100;
+            break;
+        case "medium": 
+        yieldFactor = yieldFactor * (100 + crop.factors.sun.medium) / 100;
+            break;
+        case "high": 
+        yieldFactor = yieldFactor * (100 + crop.factors.sun.high) / 100;
+    }
+    switch (environmentFactor.wind){
+        case "low": 
+        yieldFactor = yieldFactor * (100 + crop.factors.wind.low) / 100;
+            break;
+        case "medium": 
+        yieldFactor = yieldFactor * (100 + crop.factors.wind.medium) / 100;
+            break;
+        case "high": 
+        yieldFactor = yieldFactor * (100 + crop.factors.wind.high) / 100;
+    }
+    switch (environmentFactor.underground){
+        case "clay": 
+        yieldFactor = yieldFactor * (100 + crop.factors.underground.clay) / 100;
+            break;
+        case "sand": 
+        yieldFactor = yieldFactor * (100 + crop.factors.underground.sand) / 100;
+            break;
+        case "garden": 
+        yieldFactor = yieldFactor * (100 + crop.factors.underground.garden) / 100;
+    }
+    return yieldFactor;
+}
+
 // functions 
-const getYieldForPlant = (crop, environmentFactors) => getYieldFactor(crop, environmentFactors) * crop.yield;
-const getYieldForCrop = (input) => input.numCrops * input.crop.yield;
+const getYieldForPlant = (crop, environmentFactors) => getYieldFactor(crop, environmentFactors) * crop.yield ;
+const getYieldForCrop = (input, environmentFactors) => input.numCrops * input.crop.yield * getYieldFactor(input, environmentFactors);
+
 const getTotalYield = ({crops}) => {
     let yield_total = 0;
     crops.forEach(element => {
@@ -13,40 +51,87 @@ const getRevenueForCrop = (crop) => crop.numCrops * getYieldForPlant(crop) * cro
 const getProfitForCrop = (crop) => getRevenueForCrop(crop) - getCostsForCrop(crop);
 const getTotalProfit = (crop) => getRevenueForCrop(crop) - getProfitForCrop(crop);
 
-// helpers
-const getYieldFactor = (crop, environmentFactor)=>{
-    let yieldFactor = 1;
-    switch (environmentFactor.sun){
-        case "low":
-            yieldFactor = yieldFactor * (100 + crop.factors.sun.low) / 100;
-            break;
-        case "medium":
-            yieldFactor = yieldFactor * (100 + crop.factors.sun.medium) / 100;
-            break;
-        case "high":
-            yieldFactor = yieldFactor * (100 + crop.factors.sun.high) / 100;
-    }
-    switch (environmentFactor.wind){
-        case "low":
-            yieldFactor = yieldFactor * (100 + crop.factors.wind.low) / 100;
-            break;
-        case "medium":
-            yieldFactor = yieldFactor * (100 + crop.factors.wind.medium) / 100;
-            break;
-        case "high":
-            yieldFactor = yieldFactor * (100 + crop.factors.wind.high) / 100;
-    }
-    switch (environmentFactor.underground){
-        case "clay":
-            yieldFactor = yieldFactor * (100 + crop.factors.underground.clay) / 100;
-            break;
-        case "sand":
-            yieldFactor = yieldFactor * (100 + crop.factors.underground.sand) / 100;
-            break;
-        case "garden":
-            yieldFactor = yieldFactor * (100 + crop.factors.underground.garden) / 100;
-    }
-    return yieldFactor;
+
+//vegetables to sale
+
+const corn = {
+    name: "corn",
+    numberOfPlantsInCrop: 30,
+    costs: 2, 
+    yield: 3,
+    salesPrice: 3,
+    factors:{
+        sun:{
+            low: -50,
+            medium: 0,
+            high: 50,
+        },
+        wind:{
+            low: 60,
+            medium: 0,
+            high: -50,
+        },
+        underground:{
+            clay: 20,
+            sand: -10,
+            garden: 50,
+        },
+    },
+};
+let input = {
+crop: corn,
+numCrops: 10,
+}
+let environmentFactors = {
+sun: "low",
+wind: "medium",
+underground: "clay"
+};
+
+console.log('The profit for the ${input.crop.name} is in euro' + Math.floor(getTotalProfit(input, environmentFactors)));
+
+const pumpkin = {
+    name: "pumpkin",
+    numberOfPlantsInCrop: 50,
+    costs: 2,
+    yield: 2,
+    salesPrice: 4,
+    factors:{
+        sun:{
+            low: -50,
+            medium: 50,
+            high: 100,
+        },
+        wind:{
+            low: 50,
+            medium: -20,
+            high: -50,
+        },
+        underground:{
+            clay: 50,
+            sand: -50,
+            garden: 10,
+        },
+    },
+};
+input = {
+crop: pumpkin,
+numCrops: 25,
 }
 
-module.exports = {getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop, getRevenueForCrop, getProfitForCrop, getTotalProfit};
+environmentFactors = {
+sun: "high",
+wind: "medium",
+underground: "garden"
+};
+
+console.log('The profit for the ${input.crop.name} is in euro' + Math.floor(getTotalProfit(input, environmentFactors)));
+
+module.exports = {
+    getYieldForPlant, 
+    getYieldForCrop, 
+    getTotalYield, 
+    getCostsForCrop, 
+    getRevenueForCrop, 
+    getProfitForCrop, 
+    getTotalProfit};
